778bdd34a8060a9927777ddc3be3ffaee729345e
Sunny <sunny6464n@gmail.com>
2022-04-07 22:26:57 +0530

    Linking the contact button to the contact page

--- Day 7/Portfolio/javascript/index.js
+++ Day 7/Portfolio/javascript/index.js
@@ -4,3 +4,9 @@ burger.addEventListener("click", function() {
     navUl.classList.toggle('active');
     burger.classList.toggle('ham-open')
 });
+
+// Contact page button
+
+document.getElementById("contact-btn").addEventListener("click", function(){
+    location.href ="https://sunny-64.github.io/100-Days-Of-Web-Development/Day%207/Portfolio/html/contact.html"; 
+}); 
