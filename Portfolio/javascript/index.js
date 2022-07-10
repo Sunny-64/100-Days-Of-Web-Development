669562cfd750527fbbddb9bcbe4cb06591be0560
Sunny <sunny6464n@gmail.com>
2022-07-10 12:03:16 +0530

    changed the contact page link

--- Portfolio/javascript/index.js
+++ Portfolio/javascript/index.js
@@ -1,12 +1,12 @@
 navUl = document.querySelector(".right-side-nav");
 burger = document.querySelector(".ham-burger");
-burger.addEventListener("click", function() {
+burger.addEventListener("click", function () {
     navUl.classList.toggle('active');
     burger.classList.toggle('ham-open')
 });
 
 // Contact page button
 
-document.getElementById("contact-btn").addEventListener("click", function(){
-    location.href ="https://sunny-64.github.io/100-Days-Of-Web-Development/Day%207/Portfolio/html/contact.html"; 
+document.getElementById("contact-btn").addEventListener("click", function () {
+    location.href = "https://sunny-64.github.io/100-Days-Of-Web-Development/Day%207/Portfolio/html/contact.html";
 }); 
