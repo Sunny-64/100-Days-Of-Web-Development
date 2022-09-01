f8392922cb38a5366f016b03a1ed221f530d8af1
Sunny <sunny6464n@gmail.com>
2022-09-01 16:02:43 +0530

    Added login and signup form

--- to-do-list/app.js
+++ to-do-list/app.js
@@ -27,6 +27,19 @@ app.post("/", function (req, res) {
 });
 
 
+
+app.get("/login", function (req, res) {
+   
+    res.render("login");
+});
+
+app.get("/signup", function (req, res) {
+   
+    res.render("signup");
+});
+
+
+
 app.listen(80, function () {
     console.log("Server running at port 80");
 });
