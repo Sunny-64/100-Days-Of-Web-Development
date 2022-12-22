0ed6910c847e41d7ab53454b4b5351db02a19fbd
Sunny <sunny6464n@gmail.com>
2022-12-22 10:40:12 +0530

    fixed the clock

--- Analog Clock/index.js
+++ Analog Clock/index.js
@@ -8,10 +8,9 @@ setInterval(() => {
     let hrotations = 30*htime + mtime/2; 
     let mrotations = 6 * mtime; 
     let srotations = 6 * stime; 
-    
 
+    
     hours.style.transform = `rotate(${hrotations}deg)`; 
     minutes.style.transform = `rotate(${mrotations}deg)`; 
     seconds.style.transform = `rotate(${srotations}deg)`; 
-
 }, 1000);
