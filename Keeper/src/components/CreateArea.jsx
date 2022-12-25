057cbd523fc121a52975fc1065534f765dc93f7e
Sunny <sunny6464n@gmail.com>
2022-12-25 18:14:50 +0530

    Adding keeper project and Todo list made in react

---
+++ Keeper/src/components/CreateArea.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import AddIcon from '@mui/icons-material/Add';
+
+
+function CreateArea(props) {
+  const [note, setNote] = useState({
+    title: "",
+    content: ""
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote(prevNote => {
+      return {
+        ...prevNote,
+        [name]: value
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: ""
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form className="create-note">
+        <input
+          name="title"
+          onChange={handleChange}
+          value={note.title}
+          placeholder="Title"
+        />
+        <textarea
+          name="content"
+          onChange={handleChange}
+          value={note.content}
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}><AddIcon /></button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
