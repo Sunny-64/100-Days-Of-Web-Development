c254971d529ee23cfd5a5e9c74627c97f5d56598
Sunny <sunny6464n@gmail.com>
2022-12-26 11:08:27 +0530

    added background and animations

--- Keeper/src/components/CreateArea.jsx
+++ Keeper/src/components/CreateArea.jsx
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import AddIcon from '@mui/icons-material/Add';
-
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
-    content: ""
+    content: "",
   });
 
   function handleChange(event) {
     const { name, value } = event.target;
 
-    setNote(prevNote => {
+    setNote((prevNote) => {
       return {
         ...prevNote,
-        [name]: value
+        [name]: value,
       };
     });
   }
@@ -23,28 +24,38 @@ function CreateArea(props) {
     props.onAdd(note);
     setNote({
       title: "",
-      content: ""
+      content: "",
     });
     event.preventDefault();
   }
 
+  const [textareaExpanded, setTextareaExpanded] = useState(false); 
+  function expand(){
+    setTextareaExpanded(true);
+  }
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          onChange={handleChange}
-          value={note.title}
-          placeholder="Title"
-        />
+      {textareaExpanded && <input
+        name="title"
+        onChange={handleChange}
+        value={note.title}
+        placeholder="Title"
+      />}
         <textarea
           name="content"
           onChange={handleChange}
           value={note.content}
           placeholder="Take a note..."
-          rows="3"
+          onClick={expand}
+          row = {textareaExpanded ? 5 : 1} 
+          style = {textareaExpanded ? {height : "100px"} : null}
         />
-        <button onClick={submitNote}><AddIcon /></button>
+        <Zoom in={textareaExpanded ? true : false}>
+          <Fab onClick={submitNote}>
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- Keeper/src/components/Header.jsx
+++ Keeper/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import HighlightIcon from '@mui/icons-material/Highlight';
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><HighlightIcon />Keeper</h1>
     </header>
   );
 }
