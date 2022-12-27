645ae405aa7a6b6260adf14e567a04a6efcd780c
Sunny <sunny6464n@gmail.com>
2022-12-27 12:58:21 +0530

    added projects section.

--- Portfolio/javascript/index.js
+++ Portfolio/javascript/index.js
@@ -7,6 +7,28 @@ burger.addEventListener("click", function () {
 
 // Contact page button
 
-document.getElementById("contact-btn").addEventListener("click", function () {
-    location.href = "https://sunny-64.github.io/100-Days-Of-Web-Development/Day%207/Portfolio/html/contact.html";
-}); 
+// document.getElementById("contact-btn").addEventListener("click", function () {
+//     location.href = "https://sunny-64.github.io/100-Days-Of-Web-Development/Day%207/Portfolio/html/contact.html";
+// }); 
+
+let cards = document.querySelectorAll(".card");
+let viewProjects = document.querySelectorAll(".view-project"); 
+
+// cards.addEventListener("mouseover",()=>{
+//     document.querySelector(".view-project").style.opacity = "1"; 
+//     document.querySelector(".view-project").style.transform = "scale(1)"; 
+// })
+
+cards.forEach((card, index) =>{
+    card.addEventListener("mouseover", ()=>{      
+        viewProjects[index].style.opacity = "1"; 
+        viewProjects[index].style.transform = "scale(1)";
+    })
+})
+cards.forEach((card, index) =>{
+    card.addEventListener("mouseout", ()=>{      
+        viewProjects[index].style.opacity = "0"; 
+        viewProjects[index].style.transform = "scale(0.5)";
+    })
+})
+
